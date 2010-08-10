b63c0a8c99f9c90992718f3e7f8acde1eb502f6d
Wenzel Jakob <wenzel@cs.cornell.edu>
2010-08-10 18:26:11 +0200

    removed the included SCons release

--- tools/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set SCONS_LIB_DIR=.\tools\scons-1.2.0\engine
-tools\scons-1.2.0\script\scons
--- tools/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export SCONS_LIB_DIR=`pwd`/tools/scons-1.2.0/engine
-./tools/scons-1.2.0/script/scons $@
