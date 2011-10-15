7f71397b82b8038e4fb331bde3c792efdd8a0210
Wenzel Jakob <wenzel@cs.cornell.edu>
2011-10-15 01:21:51 -0400

    OSX build fix

--- src/mtsgui/symlinks_auth.cpp
+++ src/mtsgui/symlinks_auth.cpp
@@ -1,4 +1,41 @@
 #if defined(__APPLE__)
+#ifndef __MAC_10_7
+	/// Work around a few missing definitions in the MacOS X Lion header files
+    #define __MAC_10_7 1070
+
+    #if !defined(__MAC_OS_X_VERSION_MAX_ALLOWED) || (__MAC_OS_X_VERSION_MAX_ALLOWED == __MAC_10_6)
+        #ifdef __MAC_OS_X_VERSION_MAX_ALLOWED
+            #undef __MAC_OS_X_VERSION_MAX_ALLOWED
+        #endif
+        #define __MAC_OS_X_VERSION_MAX_ALLOWED __MAC_10_7
+    #endif
+
+    #if __MAC_OS_X_VERSION_MAX_ALLOWED < __MAC_10_7
+        #define __AVAILABILITY_INTERNAL__MAC_10_7        __AVAILABILITY_INTERNAL_UNAVAILABLE
+    #elif __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_7
+        #define __AVAILABILITY_INTERNAL__MAC_10_7        __AVAILABILITY_INTERNAL_WEAK_IMPORT
+    #else
+        #define __AVAILABILITY_INTERNAL__MAC_10_7        __AVAILABILITY_INTERNAL_REGULAR
+    #endif
+    #if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_7
+        #define __AVAILABILITY_INTERNAL__MAC_10_0_DEP__MAC_10_7        __AVAILABILITY_INTERNAL_DEPRECATED
+        #define __AVAILABILITY_INTERNAL__MAC_10_1_DEP__MAC_10_7        __AVAILABILITY_INTERNAL_DEPRECATED
+        #define __AVAILABILITY_INTERNAL__MAC_10_2_DEP__MAC_10_7        __AVAILABILITY_INTERNAL_DEPRECATED
+        #define __AVAILABILITY_INTERNAL__MAC_10_3_DEP__MAC_10_7        __AVAILABILITY_INTERNAL_DEPRECATED
+        #define __AVAILABILITY_INTERNAL__MAC_10_4_DEP__MAC_10_7        __AVAILABILITY_INTERNAL_DEPRECATED
+        #define __AVAILABILITY_INTERNAL__MAC_10_5_DEP__MAC_10_7        __AVAILABILITY_INTERNAL_DEPRECATED
+    #else
+        #define __AVAILABILITY_INTERNAL__MAC_10_0_DEP__MAC_10_7        __AVAILABILITY_INTERNAL__MAC_10_0
+        #define __AVAILABILITY_INTERNAL__MAC_10_1_DEP__MAC_10_7        __AVAILABILITY_INTERNAL__MAC_10_1
+        #define __AVAILABILITY_INTERNAL__MAC_10_2_DEP__MAC_10_7        __AVAILABILITY_INTERNAL__MAC_10_2
+        #define __AVAILABILITY_INTERNAL__MAC_10_3_DEP__MAC_10_7        __AVAILABILITY_INTERNAL__MAC_10_3
+        #define __AVAILABILITY_INTERNAL__MAC_10_4_DEP__MAC_10_7        __AVAILABILITY_INTERNAL__MAC_10_4
+        #define __AVAILABILITY_INTERNAL__MAC_10_5_DEP__MAC_10_7        __AVAILABILITY_INTERNAL__MAC_10_5
+    #endif
+    #define __AVAILABILITY_INTERNAL__MAC_10_7_DEP__MAC_NA             __AVAILABILITY_INTERNAL__MAC_10_7
+
+#endif  /* !defined(__MAC_10_7) */
+
 #include <Authorization.h>
 #include <AuthorizationTags.h>
 #include <unistd.h>
@@ -11,7 +48,6 @@ namespace mitsuba {
 bool create_symlinks() {
 	AuthorizationFlags flags = kAuthorizationFlagDefaults;
 	AuthorizationRef ref;
-
 	OSStatus status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, flags, &ref);
 	if (status != errAuthorizationSuccess)
 		return false;
