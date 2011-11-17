93214e4cc22f83088526cc0f0138d7ff414db8f0
Wenzel Jakob <wenzel@cs.cornell.edu>
2011-11-17 14:41:54 -0500

    bugfix for the build scripts when Qt is inactive

--- setpath.sh
+++ setpath.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-unamestr=`uname`
-if [[ "$unamestr" == 'Darwin' ]]; then
-	export PATH=`pwd`/Mitsuba.app/Contents/MacOS:$PATH
-else
-	export LD_LIBRARY_PATH=`pwd`/dist
-	export PATH=`pwd`/dist:$PATH
-	ulimit -c 1000000000
-fi
+export PATH=`pwd`/dist:/hydra/S1/local/bin:/hydra/S1/local/python271/bin:$PATH
+export LD_LIBRARY_PATH=`pwd`/dist:/hydra/S1/local/gcc461/lib64:`pwd`/dist:/hydra/S1/local/gcc461/lib:/hydra/S1/local/python271/lib/:/hydra/S1/local/mitsuba/lib/:/hydra/S1/local/mitsuba/lib64/:$LD_LIBRARY_PATH
+
