a0c20094a16966963020c130353b00b839e28e55
Wenzel Jakob <wenzel@cs.cornell.edu>
2013-04-06 20:12:44 -0700

    made clipped AABB computation numerically more robust

--- src/shapes/cylinder.cpp
+++ src/shapes/cylinder.cpp
@@ -375,10 +375,10 @@ public:
 		/* Find the componentwise maxima of the ellipse */
 		for (int i=0; i<2; ++i) {
 			int j = (i==0) ? axis1 : axis2;
-			Float alpha = ellipseAxes[0][j];
-			Float beta = ellipseAxes[1][j];
-			Float ratio = beta/alpha, tmp = std::sqrt(1+ratio*ratio);
-			Float cosTheta = 1/tmp, sinTheta = ratio/tmp;
+			Float alpha = ellipseAxes[0][j], beta = ellipseAxes[1][j];
+			Float tmp = 1 / std::sqrt(alpha*alpha + beta*beta);
+			Float cosTheta = alpha * tmp, sinTheta = beta*tmp;
+			
 			Point p1 = ellipseCenter + cosTheta*ellipseAxes[0] + sinTheta*ellipseAxes[1];
 			Point p2 = ellipseCenter - cosTheta*ellipseAxes[0] - sinTheta*ellipseAxes[1];
 
