010c78bb229df3df4574ef098fbd71649d2b9591
L. E. Segovia <amyspark@users.noreply.github.com>
2017-09-16 10:36:00 -0300

    Add MSVC2017 build config and project Porting the VS solution also enables debug running via NMake; fixes #24

--- build/config-win64-msvc2017.py
+++ build/config-win64-msvc2017.py
@@ -5,13 +5,18 @@ CC              = 'cl'
 # /O2=optimize for speed, global optimizations, intrinsic functions, favor fast code, frame pointer omission
 # /EHsc=C++ exceptions, /fp:fast=Enable reasonable FP optimizations, /GS-=No buffer security checks, /GL=whole program optimizations
 # To include debug information add '/Z7' to CXXFLAGS and '/DEBUG' to LINKFLAGS
-CXXFLAGS        = ['/nologo', '/O2', '/fp:fast', '/D', 'WIN32', '/D', 'WIN64', '/W3', '/EHsc', '/GS-', '/GL', '/MD', '/D', 'MTS_DEBUG', '/D', 'SINGLE_PRECISION', '/D', 'SPECTRUM_SAMPLES=3', '/D', 'MTS_SSE', '/D', 'MTS_HAS_COHERENT_RT', '/D', '_CONSOLE', '/D', 'NDEBUG', '/D', 'OPENEXR_DLL', '/openmp']
+# EDIT: MSVC2017 bugs Mitsuba when using /O2 because of /Ob2 - replaced it with the full set and /Ob2 for /Ob1
+# EDIT: /Og is deprecated too
+CXXFLAGS        = ['/nologo', #'/Og',
+'/Oi', '/Ot', '/Oy', '/Ob1', #'/Ob2',
+'/Gs', '/GF', '/Gy', #'/O2',
+'/fp:fast', '/D', 'WIN32', '/D', 'WIN64', '/W3', '/EHsc', '/GS-', '/GL', '/MD', '/D', 'MTS_DEBUG', '/D', 'SINGLE_PRECISION', '/D', 'SPECTRUM_SAMPLES=3', '/D', 'MTS_SSE', '/D', 'MTS_HAS_COHERENT_RT', '/D', '_CONSOLE', '/D', 'NDEBUG', '/D', 'OPENEXR_DLL', '/openmp']
 SHCXXFLAGS      = CXXFLAGS
 TARGET_ARCH     = 'x86_64'
 MSVC_VERSION    = '15.0'
 LINKFLAGS       = ['/nologo', '/SUBSYSTEM:CONSOLE', '/MACHINE:X64', '/FIXED:NO', '/OPT:REF', '/OPT:ICF', '/LTCG', '/NODEFAULTLIB:LIBCMT', '/MANIFEST']
 BASEINCLUDE     = ['#include', '#dependencies/include']
-BASELIB         = ['msvcrt', 'ws2_32', 'Half']
+BASELIB         = ['msvcrt', 'ws2_32', 'Half', 'zlib']
 BASELIBDIR      = ['#dependencies/lib']
 OEXRINCLUDE     = ['#dependencies/include/openexr']
 OEXRLIB         = ['IlmImf', 'IlmThread', 'Iex', 'zlib']
@@ -33,5 +38,6 @@ PYTHON35LIB     = ['boost_python35-vc141-mt-1_64', 'python35']
 PYTHON35INCLUDE = ['#dependencies/include/python35']
 PYTHON36LIB     = ['boost_python36-vc141-mt-1_64', 'python36']
 PYTHON36INCLUDE = ['#dependencies/include/python36']
+QTINCLUDE       = ['#dependencies/include']
 QTDIR           = '#dependencies'
 FFTWLIB         = ['libfftw3-3']
