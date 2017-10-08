beb2e07575986838e6e9be5f9cc103d79c539cfd
L. E. Segovia <amyspark@users.noreply.github.com>
2017-10-08 13:46:08 -0300

    Fix COLLADA DOM library name

--- build/config-linux-gcc-debug.py
+++ build/config-linux-gcc-debug.py
@@ -19,8 +19,8 @@ XERCESLIB      = ['xerces-c']
 GLLIB          = ['GL', 'GLU', 'GLEWmx', 'Xxf86vm', 'X11']
 GLFLAGS        = ['-DGLEW_MX']
 BOOSTLIB       = ['boost_system', 'boost_filesystem', 'boost_thread']
-COLLADAINCLUDE = ['/usr/include/collada-dom', '/usr/include/collada-dom/1.4']
-COLLADALIB     = ['collada14dom', 'xml2']
+COLLADAINCLUDE = ['/usr/include/collada-dom2.4', '/usr/include/collada-dom2.4/1.4']
+COLLADALIB     = ['collada-dom2.4-dp']
 FFTWLIB        = ['fftw3_threads', 'fftw3']
 
 # The following runs a helper script to search for installed Python
